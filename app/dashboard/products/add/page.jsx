192863b46878862e1f63f3d770cdad0fc8387bfa
arguptaex <architgupta.rke@gmail.com>
2024-07-23 14:49:42 +0530

    commit loading button added in product add form

--- app/dashboard/products/add/page.jsx
+++ app/dashboard/products/add/page.jsx
@@ -1,11 +1,33 @@
 // import React from 'react'
+"use client";
 import { addProduct } from "@/app/lib/actions";
 import styles from "@/components/dashboard/products/addProduct/addProducts.module.css";
+// import { CLoadingButton } from "@coreui/react-pro";
+import { useState } from "react";
 const AddProductPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const formData = new FormData(event.target);
+    // const data = Object.fromEntries(formData);
+
+    try {
+      await addProduct(formData);
+    } catch (error) {
+      console.error("Error adding product:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <section className="add-product-section">
       <div className={styles.container}>
-        <form action={addProduct} className={styles.form}>
+        {/* <form action={addProduct} className={styles.form}> */}
+        <form onSubmit={handleSubmit} className={styles.form}>
           <input type="text" placeholder="title" name="title" required />
           <select name="category" id="category">
             <option value="general">Choose a category</option>
@@ -24,7 +46,13 @@ const AddProductPage = () => {
             rows={16}
             placeholder="Description"
           ></textarea>
-          <button type="submit">Submit</button>
+          {/* <button type="submit">Submit</button> */}
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? "Loading..." : "Submit"}
+          </button>
+          {/* <CLoadingButton color="info" spinnerType="grow" timeout={2000}>
+            closing button
+          </CLoadingButton> */}
         </form>
       </div>
     </section>
